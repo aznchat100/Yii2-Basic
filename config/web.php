15b71577f45bf6c062286e50fc91825ccb9ed988
billcheng <billcheng@rchis-iMac.ad.tamhsc.edu>
2015-07-06 09:32:02 -0500

    defaultRoute in web.php

--- config/web.php
+++ config/web.php
@@ -4,6 +4,10 @@ $params = require(__DIR__ . '/params.php');
 
 $config = [
     'id' => 'basic',
+    /*
+    You may change the default controller with the following application configuration:
+    'defaultRoute' => 'site/say',
+    */
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'components' => [
